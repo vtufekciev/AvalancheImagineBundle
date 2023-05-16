91cdf225248a66d1aded5da841a28d1ef1872a76
Vladimir Tufekchiev <vtufekciev@gmail.com>
2023-05-16 19:04:09 +0300

    Update ImagineExtension.php
    compatible with 2.7 symfony

--- Templating/ImagineExtension.php
+++ Templating/ImagineExtension.php
@@ -29,7 +29,7 @@ class ImagineExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            'apply_filter' => new \Twig_Filter_Method($this, 'applyFilter'),
+            'apply_filter' => new \Twig_SimpleFilter($this, 'applyFilter'),
         );
     }
 
